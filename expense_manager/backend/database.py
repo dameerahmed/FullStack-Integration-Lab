4dea32f6cc7927033d5b7a5c462caedf3888d92e
dameerahmed <dameerahmedghouri05@gmail.com>
2025-12-09 21:39:12 -0800

    second commit

--- expense_manager/backend/database.py
+++ expense_manager/backend/database.py
@@ -1,11 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-engine = create_engine("sqlite:///./expense.db", connect_args={"check_same_thread": False})
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 Base = declarative_base()
